ed790ee3bbc8d6cb2b49c94af79e607afc75b48e
victoriaschwin <vicky.schwint95@gmail.com>
2022-12-12 14:49:30 +0100

    setup & iterat 1

---
+++ setup/src/components/Component.vue
@@ -0,0 +1,15 @@
+<template>
+ <p>New Component</p>
+</template>
+
+<script>
+export default {
+
+  }
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
